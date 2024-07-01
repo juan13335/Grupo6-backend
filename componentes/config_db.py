03e055cb3a8334d6e3cbda365f868ea6f6d8a45a
juan13335 <juanpablofarias26@gmail.com>
2024-07-01 14:27:33 +0000

    arreglo_pew

--- componentes/config_db.py
+++ componentes/config_db.py
@@ -1,10 +1,10 @@
-import mysql.connector 
+import mysql.connector
 
 config_db = {
-    "user" : 'root',
-    "password" : 'Ritarita1010',
-    "host" : '127.0.0.1',
-    "database" : 'noticias',
+    "user" : 'juan13335',
+    "password" : 'grupo6backend',
+    "host" : 'juan13335.mysql.pythonanywhere-services.com',
+    "database" : 'juan13335$noticiero',
 }
 
 conexion = mysql.connector.connect(**config_db)
