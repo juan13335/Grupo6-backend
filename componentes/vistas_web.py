f2e4f5a7b524b6a8b982fa4c8ab057c1f0cdcb0e
juan13335 <juanpablofarias27@gmail.com>
2024-07-02 20:50:27 -0300

    cualquier cosa

--- componentes/vistas_web.py
+++ componentes/vistas_web.py
@@ -1,7 +1,23 @@
-from flask import render_template
+from flask import render_template, request
+#import mysql.connector
+
 
 from main import app
 
 @app.route('/')
 def inicio():
     return render_template('./index.html')
+
+#@app.route('/', methods=['GET', 'POST'])
+#def crear_registro():
+    nombre = request.form['nombre']
+    apellido = request.form['apellido']
+    email = request.form['email']
+    password = request.form['password']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO usuario (usuario, apellido, email, password) VALUES (%s, %s, %s, %s)", (nombre, apellido, email, password))
+    mysql.connection.commit()
+    return render_template("index.html")
+
+
+
